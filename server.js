2ff7a68e1c0149f1d0e12bd4f1691748d0725bbc
Moroni Allred <moroniallred@gmail.com>
2019-08-21 15:36:03 -0600

    set up

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express")
 const app = express()
+require("dotenv").config()
 const morgan = require("morgan")
 const mongoose = require("mongoose")
 const PORT = process.env.PORT || 7000
+const path = require("path")
 
 app.use(morgan("dev"))
 app.use(express.json())
+app.use(express.static(path.join(__dirname, "client", "build")))
 
-mongoose.connect("mongodb://localhost:27017/employeeDB",{
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/employeeDB",{
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true 
@@ -21,6 +25,10 @@ app.use((err, req, res, next) => {
         console.log(err)
         return res.send({errMsg: err.message})
 })
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
         console.log(`server is running on ${PORT}`)
